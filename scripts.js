1f83f2a6353015cf8a571ce345c1a4df58f89366
Radek Izdebski <radek1izdebski@gmail.com>
2018-02-24 12:17:42 +0100

    Change js file

--- scripts.js
+++ scripts.js
@@ -2,14 +2,13 @@ function getTriangleArea(a, h) {
 	  if (( a <= 0 ) || ( h <= 0 )) {
 		console.log('Incorrect data!');
 	} else {
-	  	return triangleArea = a * h / 2;
+	  	return(a * h / 2);
 	}
 }
 console.log(getTriangleArea(10, 6));
-console.log(getTriangleArea (9, 0));
+console.log(getTriangleArea(9, 0));
 
-var triangle1Area = getTriangleArea(9, 13);
+var triangle1Area = getTriangleArea(9, 13),
 	triangleArea2 = getTriangleArea(22, 76),
 
-	console.log(triangleArea2);
-
+	console.log(triangleArea2)
